847aed77e1e3447e949820ca58bdeb02ee2f19e9
deadseal001 <110753777+deadseal001@users.noreply.github.com>
2023-03-22 09:43:58 -0500

    update resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 import {DiHtml5, DiJavascript1, DiReact, DiNodejs, DiMongodb, DiGit, DiHeroku} from "react-icons/di";
 import {SiMysql,SiTailwindcss} from "react-icons/si";
 import {BsFillBootstrapFill} from "react-icons/bs";
-import resumed from "../../assets/Wenbo_Li_CV.pdf"
+import resumed from "../../assets/Wenbo_Li_resume.pdf"
 
 
 function Resume() {
